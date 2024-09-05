1a78e3e99964099a0d97622302c3aef0de439686
Client4894 <lucas.richers@pikoworks.de>
2024-09-05 21:27:40 +0200

    frame rate limitationn

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
 
+
 def main():
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black", rect=None, special_flags=0)
+
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
