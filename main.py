88a9872e993e017db2752a808ab43ef00154fba9
Client4894 <lucas.richers@pikoworks.de>
2024-09-22 19:21:56 +0200

    group containers added

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -14,10 +21,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-        
-        screen.fill("black") # 1. Step - Clear all
-        player.draw(screen) # 2. Step - Draw everything
+        for obj in updateable:
+            obj.update(dt)
+
+        screen.fill("black") # 1. Step - Clear all        
+
+        for obj in drawable:
+            obj.draw(screen) # 2. Step - Draw everything
+
         pygame.display.flip() # 3. Step - Update display
 
         # limit the framerate to 60 FPS
